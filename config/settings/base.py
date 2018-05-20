c7a6ee6e1ace84ec7822387d10c7ce0996973732
Solitasroh <solitasroh@gmail.com>
2018-05-20 15:00:24 +0900

    user model 확장

--- config/settings/base.py
+++ config/settings/base.py
@@ -71,6 +71,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'nomadgram.users.apps.UsersConfig',
+    'nomadgram.images.apps.ImagesConfig'
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
