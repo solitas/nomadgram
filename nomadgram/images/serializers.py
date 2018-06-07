0c30f24cd6e33b353e3543942531d072bb739e7a
Solitasroh <solitasroh@gmail.com>
2018-06-07 23:07:37 +0900

    Serializing the Feed and getting image_id from URL

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
 from . import models
+from nomadgram.users import models as user_models
 
+class FeedUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = user_models.User;
+        fields = (
+            'username',
+            'profile_image'
+        )
 
 class CommentSerializer(serializers.ModelSerializer):
 
+    creator = FeedUserSerializer()
+
     class Meta:
         model = models.Comment
-        fields = '__all__'
+        fields = (
+            'id',
+            'message',
+            'creator'
+        )
 
 class LikeSerializer(serializers.ModelSerializer):
 
@@ -16,9 +31,15 @@ class LikeSerializer(serializers.ModelSerializer):
 
 class ImageSerializer(serializers.ModelSerializer):
 
+    creator = FeedUserSerializer()
     comments = CommentSerializer(many=True)
-    likes = LikeSerializer(many=True)
 
     class Meta:
         model = models.Image
-        fields = '__all__'
+        fields = (
+            'location',
+            'caption',
+            'comments',
+            'like_count',
+            'creator'
+        )
