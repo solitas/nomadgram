b6156407db795e1058327bc99cff1f11d40e4f11
Solitasroh <solitasroh@gmail.com>
2018-06-10 22:34:15 +0900

    search user

--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -7,11 +7,7 @@ urlpatterns = [
     url(regex=r"^explore/$", view=views.ExploreUsers.as_view(), name="explore_users"),
     url(regex=r'^(?P<user_id>[0-9]+)/follow/$', view=views.FollowUser.as_view(), name="follow_user"),
     url(regex=r'^(?P<user_id>[0-9]+)/unfollow/$', view=views.UnFollowUser.as_view(), name="unfollow_user"),
-    url(
-        regex=r'^(?P<username>\w+)/$', 
-        view=views.UserProfile.as_view(), 
-        name='user_profile'
-    ),
+    
     url(
         regex=r'^(?P<username>\w+)/followers/$', 
         view=views.UserFollowers.as_view(), 
@@ -21,5 +17,15 @@ urlpatterns = [
         regex=r'^(?P<username>\w+)/following/$', 
         view=views.UserFollowing.as_view(),
         name='user_following'
+    ),
+    url(
+        regex=r'^search/$',
+        view=views.Search.as_view(),
+        name='search_user'
+    ),
+    url(
+        regex=r'^(?P<username>\w+)/$', 
+        view=views.UserProfile.as_view(), 
+        name='user_profile'
     ),
 ]
