8df3fcaf229ad44a55a2fae583ab5ebd386ad0fa
Solitasroh <solitasroh@gmail.com>
2018-06-24 12:11:15 +0900

    Setting Up The Redux Store with Multiple Reducers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
+
 import './index.css';
 import App from './App';
 
+const store = configureStore();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
 
